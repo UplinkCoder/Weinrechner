674d74fe1056db54327b0499ceb775378d84c1fb
Uplink.Coder <uplink.coder@gmail.com>
2013-11-30 13:49:54 +0100

    Fixed Template eintragen.dt build first scaffolding for later aplication

--- source/app.d
+++ source/app.d
@@ -1,12 +1,21 @@
 module source.app;
 
-import object;
+import vibe.d;
 
-class app
+void handleRequest(HTTPServerRequest req,
+                   HTTPServerResponse res)
 {
-	this()
-	{
-		// Constructor code
-	}
+	res.writeBody("Hello, World!", "text/plain");
 }
 
+shared static this()
+{
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	auto router = new URLRouter;
+	router.get("/", (req,res){return res.redirect("/index.html");});
+	router.get("/index.html",
+		(req,res){return res.redirect("/frame/eintragen.html");});
+	router.get("/frame/eintragen.html",staticTemplate!("eintragen.dt"));
+	listenHTTP(settings, router);
+}
