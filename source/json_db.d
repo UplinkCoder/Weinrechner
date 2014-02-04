c54e7bff5d0b3e3b01c04354c5516f1903961660
Uplink_Coder <uplink.coder@gmail.com>
2014-02-04 12:11:43 +0100

    added debug info

--- source/json_db.d
+++ source/json_db.d
@@ -20,6 +20,8 @@ void putJson(HTTPServerRequest req,
 		void[] buffer = std.file.read("wines.json");
 		string str_wines_json = buffer.to!string;
 		string  formdatajson_str = "[" ~ req.form.serializeToJson.to!string ~ "]";
+		logDebug("I got :\n\""~formdatajson_str~"\"\n form server\n");
+		logDebug("I have :\n\""~str_wines_json~"\"\n in my wines.json\n"); 
 		if (str_wines_json != "") {  
 			str_wines_json = (parseJson(str_wines_json) ~ parseJsonString(formdatajson_str)).to!string;
 		} else {
